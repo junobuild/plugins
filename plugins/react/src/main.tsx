1290232cf22d37598383bd10cec2231496cad76f
David Dal Busco <david.dalbusco@outlook.com>
2025-01-25 10:00:58 +0100

    chore: move react contribution (#10)

--- plugins/react/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Wrapper from './wrapper';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Wrapper>
-    <App />
-  </Wrapper>
-);
