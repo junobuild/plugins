9e2a0b57239b7a4b723b70b6b5e7c612cd95b600
David Dal Busco <david.dalbusco@outlook.com>
2025-12-10 13:16:12 +0100

    chore: single esbuild script (#54)

--- plugins/nextjs-plugin/esbuild.mjs
+++ plugins/nextjs-plugin/esbuild.mjs
@@ -1,46 +1,5 @@
 #!/usr/bin/env node
 
-import esbuild from 'esbuild';
-import {existsSync, mkdirSync, readFileSync} from 'fs';
-import {join} from 'path';
-
-const dist = join(process.cwd(), 'dist');
-
-const createDistFolder = () => {
-  if (!existsSync(dist)) {
-    mkdirSync(dist);
-  }
-};
-
-// Skip peer dependencies
-const peerDependencies = (packageJson) => {
-  const json = readFileSync(packageJson, 'utf8');
-  const {peerDependencies} = JSON.parse(json);
-  return peerDependencies ?? {};
-};
-
-const workspacePeerDependencies = peerDependencies(join(process.cwd(), 'package.json'));
-
-const buildEsm = () => {
-  esbuild
-    .build({
-      entryPoints: ['src/index.ts'],
-      outfile: 'dist/index.mjs',
-      bundle: true,
-      minify: true,
-      format: 'esm',
-      platform: 'node',
-      banner: {
-        js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);"
-      },
-      external: [...Object.keys(workspacePeerDependencies)]
-    })
-    .catch(() => process.exit(1));
-};
-
-export const build = () => {
-  createDistFolder();
-  buildEsm();
-};
+import {build} from '../../scripts/esbuild.mjs';
 
 build();
